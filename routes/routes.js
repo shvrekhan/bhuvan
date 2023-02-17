b6e68cbcf9431617fe2a38b0c56559985a3d0f62
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 16:12:03 +0530

    router.put and router.delete added in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,7 @@ const controller = require("../controller/controller");
 
 router.get("/getNames", controller.get);
 router.post("/postName", controller.post);
+router.put("/:id", controller.put);
+router.delete("/:id", controller.delete);
 
 module.exports = router;
