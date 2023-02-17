20a68d9a13641ba4a5ca52145a08349aad9cd831
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 13:25:47 +0530

    port in imported from config.js in server.js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const routes = require("./routes/routes")
-require('dotenv').config()
+const routes = require("./routes/routes");
+const {port} = require('./config');
 
 const app = express();
-const port = process.env.PORT;
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/',routes)
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
