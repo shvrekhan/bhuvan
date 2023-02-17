2dfb561c8cfd3116c25325ef0c59701e85a49061
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 17:14:44 +0530

    promises are created in username.model.js

--- models/username.model.js
+++ models/username.model.js
@@ -4,53 +4,61 @@ let UserName = function (userName) {
   this.name = userName.name;
 };
 
-UserName.get = (result) => {
-  dbConn.query("SELECT * FROM testAppTable", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      console.log("names: ", res);
-      result(null, res);
-    }
-  });
-};
-
-UserName.create = (newName, result) => {
-  dbConn.query("INSERT INTO testAppTable SET ?", newName, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      console.log("created name: ", { ...newName });
-      result(null, { ...newName });
-    }
-  });
-};
-
-UserName.put = (id, name, result) => {
-  dbConn.query(
-    "UPDATE testAppTable SET name=? WHERE id = ?",
-    [name, id],
-    (err, res) => {
+UserName.get = () => {
+  return new Promise((resolve, reject) => {
+    dbConn.query("SELECT * FROM testAppTable", (err, res) => {
       if (err) {
         console.log("error: ", err);
-        result(err, null);
+        reject(err);
       } else {
-        result(null, res);
+        console.log("names: ", res);
+        resolve(res);
       }
-    }
-  );
+    });
+  });
 };
 
-UserName.delete = (id, result) => {
-  dbConn.query("DELETE FROM testAppTable WHERE id=?", id, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      result(null, res);
-    }
+UserName.create = (newName) => {
+  return new Promise((resolve, reject) => {
+    dbConn.query("INSERT INTO testAppTable SET ?", [newName], (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        reject(err);
+      } else {
+        console.log("created name: ", { ...newName });
+        resolve({ ...newName });
+      }
+    });
+  });
+};
+
+UserName.put = (id, name) => {
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      "UPDATE testAppTable SET name=? WHERE id = ?",
+      [name, id],
+      (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      }
+    );
+  });
+};
+
+UserName.delete = (id) => {
+  return new Promise((resolve, reject) => {
+    dbConn.query("DELETE FROM testAppTable WHERE id=?", [id], (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
   });
 };
 
