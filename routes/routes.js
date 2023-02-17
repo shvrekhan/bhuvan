afd688585295680ec2743380f909133921d0ceef
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 15:00:45 +0530

    controller is imported in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const UserName = require("../models/username.model")
+const controller = require("../controller/controller");
 
-router.get("/getNames", (req, res) => {
-  UserName.get((err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Some error occurred.",
-      });
-    else res.send(data);
-  });
-});
- 
-router.post("/postName", (req, res) => {
-  if (!req.body) {
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  }
-  const name = {
-    name: req.body.name,
-  };
-  UserName.create(name, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Some error occurred.",
-      });
-    else res.send(data);
-  });
-});
+router.get("/getNames", controller.get);
+router.post("/postName", controller.post);
 
 module.exports = router;
