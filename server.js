5768c9740bd512c09853c2f3462bec67c26c641c
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 14:59:40 +0530

    dummy lines removed in server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const routes = require("./routes/routes");
-const {port} = require('./config');
+const { port } = require("./config");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", routes);
