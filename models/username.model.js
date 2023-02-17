91ac0f924543411554860479ef37e25200cbb661
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 16:11:42 +0530

    UserName.put and UserName.delete is added in username.model.js

--- models/username.model.js
+++ models/username.model.js
@@ -28,4 +28,30 @@ UserName.create = (newName, result) => {
   });
 };
 
+UserName.put = (id, name, result) => {
+  dbConn.query(
+    "UPDATE testAppTable SET name=? WHERE id = ?",
+    [name, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
+
+UserName.delete = (id, result) => {
+  dbConn.query("DELETE FROM testAppTable WHERE id=?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
 module.exports = UserName;
