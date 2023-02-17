81177541fa060c81b42198f3516d60b995df669b
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 17:13:46 +0530

    callback functions are converted into promises in controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,13 @@
 const UserName = require("../models/username.model");
 
 exports.get = (req, res) => {
-  UserName.get((err, data) => {
-    if (err) {
-      res.status(500).send({
-        message: err.message || "Some error occurred.",
-      });
-    } else {
+  UserName.get()
+    .then((data) => {
       res.send(data);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message || "Some error occurred." });
+    });
 };
 
 exports.post = (req, res) => {
@@ -21,40 +19,42 @@ exports.post = (req, res) => {
   const newName = {
     name: req.body.name,
   };
-  UserName.create(newName, (err, data) => {
-    if (err) {
+  UserName.create(newName)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
       res.status(500).send({
         message: err.message || "Some error occurred.",
       });
-    } else {
-      res.send(data);
-    }
-  });
+    });
 };
 
 exports.put = (req, res) => {
-  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+  if (!req.body || Object.keys(req.body).length === 0) {
     res.status(400).send({
       error: true,
-      message: "Please provide all required field",
+      message: "Content can not be empty!",
     });
   } else {
-    UserName.put(req.params.id, req.body.name, function (err, queryRes) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send({ error: false, message: "Employee successfully updated" });
-      }
-    });
+    UserName.put(req.params.id, req.body.name)
+      .then(() =>
+        res.send({
+          error: false,
+          message: "Name successfully updated",
+        })
+      )
+      .catch((err) => res.send(err));
   }
 };
 
 exports.delete = (req, res) => {
-  UserName.delete(req.params.id, (err, queryRes) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send({ error: false, message: "Employee successfully deleted" });
-    }
-  });
+  UserName.delete(req.params.id)
+    .then(() =>
+      res.send({
+        error: false,
+        message: "Name Successfully deleted",
+      })
+    )
+    .catch((err) => res.send(err));
 };
