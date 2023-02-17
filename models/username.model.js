9dc6c41cb8c90656a88dd1ec21d800c2fdbfd603
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-02-17 15:00:26 +0530

    bestpractices are followed in username.model

--- models/username.model.js
+++ models/username.model.js
@@ -5,30 +5,27 @@ let UserName = function (userName) {
 };
 
 UserName.get = (result) => {
-    dbConn.query("SELECT * FROM testAppTable", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
+  dbConn.query("SELECT * FROM testAppTable", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
       console.log("names: ", res);
       result(null, res);
-    });
-  };
+    }
+  });
+};
 
 UserName.create = (newName, result) => {
-    dbConn.query("INSERT INTO testAppTable SET ?", newName, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      console.log("created name: ", { ...newName});
-      result(null, {  ...newName });
-    });
-  };
+  dbConn.query("INSERT INTO testAppTable SET ?", newName, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      console.log("created name: ", { ...newName });
+      result(null, { ...newName });
+    }
+  });
+};
 
-
-
-module.exports=UserName
+module.exports = UserName;
